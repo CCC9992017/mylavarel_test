2027e3cbbd72f1c9aec6ea073b28726abd806c49
vagrant <vagrant@homestead>
2017-12-23 11:30:49 +0000

    视图

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('home','StaticPageController@home');
+Route::get('/help','StaticPageController@help');
+Route::get('/about','StaticPageController@about');
